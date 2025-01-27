4bc8e60ad44ccd9e3d1e05b977274d3ea72c4c26
Yadneshnaik <naikyadnesh9@gmail.com>
2025-01-27 19:43:51 +0530

    init

--- src/components/AboutMePage.jsx
+++ src/components/AboutMePage.jsx
@@ -8,9 +8,17 @@ const AboutMePage = () => {
       <Row>
         <Col>
           <p>
-            I'm a self-taught web developer with a passion for creating
-            beautiful and functional websites. I specialize in front-end
-            development and love solving complex problems.
+          Hello, I'm a self-taught web developer with a strong passion for creating beautiful 
+          and functional websites. My journey into web development began with curiosity and a 
+          drive to learn. Over time, I've honed my skills and become specialized in front-end 
+          development. I love the challenge of transforming design concepts into interactive and 
+          user-friendly interfaces.
+          <br />
+          <br />
+          Through dedication and perseverance, I've mastered a variety of web technologies such as HTML, CSS, 
+          JavaScript, and React.js. I thrive on solving complex problems, whether it's building responsive layouts 
+          or improving the performance of web applications. Each project I work on excites me, as it allows me to 
+          push the boundaries of what's possible on the web.
           </p>
         </Col>
       </Row>
